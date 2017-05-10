d193f06b57112188aee75d85eb87741d908a1e9f
Travis Neel <travis.neel1@g.austincc.edu>
2017-05-10 18:01:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,23 @@ def signIn():
                 username='USERNAME')
 return r
 
+def process_submission(submission):
+    # Ignore titles with more than 10 words as they probably are not simple
+    # questions.
+    if len(submission.title.split()) > 10:
+        return
+
+    normalized_title = submission.title.lower()
+    for question_phrase in QUESTIONS:
+        if question_phrase in normalized_title:
+            url_title = quote_plus(submission.title)
+            reply_text = REPLY_TEMPLATE.format(url_title)
+            print('Replying to: {}'.format(submission.title))
+            submission.reply(reply_text)
+            # A reply has been made so do not attempt to match other phrases.
+            break
+
+
+if __name__ == '__main__':
+    main()
+
